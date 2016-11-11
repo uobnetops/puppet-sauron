328aebeae74c63e443ea82d4e814b1f30e6a4b20
Paul Seward <paul.seward@bristol.ac.uk>
2016-11-11 10:33:30 +0000

    lint fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -143,6 +143,7 @@ class sauron (
   $sauron_fgcolor        = $sauron::params::sauron_fgcolor,
   $sauron_bgcolor        = $sauron::params::sauron_bgcolor,
   $sauron_rhf            = $sauron::params::sauron_rhf,
+  $utils_package         = $sauron::params::$utils_package,
 ) inherits sauron::params {
 
   # Configure postgres - TODO
